92e6c92ef07b822aba34ca54668cfbdfdcaf1cf4
alexandrefnsc <alexandre.fnsc@gmail.com>
2016-06-16 18:00:01 -0300

    adequacao smartphone

--- js/pessoal.js
+++ js/pessoal.js
@@ -1,24 +1,38 @@
 $(document).ready(function(){
 	$(window).scroll(function (event) {
-		var scroll = $(window).scrollTop();
-		if (scroll > 400) {
-			$('#faux-menu').slideDown();
-		}else{
-			$('#faux-menu').slideUp();
+		var scroll = $(this).scrollTop();
+		if (scroll >180) {
+			$("#faux-menu").addClass('visible');
+		} else{
+			$("#faux-menu").removeClass('visible');
 		};
 	});
 	// MENU
 	$(".menu-home").click(function(){
-		$('body,html').animate({
-			scrollTop: $("#header").offset().top
-		}, 1500,"easeInOutQuint");
-		return false;
+		if ($(window).width()<=479) {
+			$('body,html').animate({
+				scrollTop: $("#header").offset().top
+			}, 1500,"easeInOutQuint");
+			return false;
+		} else{
+			$('body,html').animate({
+				scrollTop: $("#header").offset().top
+			}, 1500,"easeInOutQuint");
+			return false;
+		};
 	});
 	$(".menu-quem-somos").click(function(){
-		$('body,html').animate({
-			scrollTop: $("#quem-somos").offset().top-140
-		}, 1500,"easeInOutQuint");
-		return false;
+		if ($(window).width()<=479) {
+			$('body,html').animate({
+				scrollTop: $("#quem-somos").offset().top
+			}, 1500,"easeInOutQuint");
+			return false;
+		} else{
+			$('body,html').animate({
+				scrollTop: $("#quem-somos").offset().top-140
+			}, 1500,"easeInOutQuint");
+			return false;
+		};
 	});
 	$(".menu-pre-moldados").click(function(){
 		$('body,html').animate({
